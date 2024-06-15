c6ca0baa13384d61639200b674e1e6006f576984
gabichagassantos@hotmail.com <gustavosilvah37@gmail.com>
2024-06-15 15:57:42 -0300

    feat: app to test de deploy created

---
+++ src/main/java/br/edu/senaisp/MedApi_Deployment/MedApiDeploymentApplication.java
@@ -0,0 +1,13 @@
+package br.edu.senaisp.MedApi_Deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApiDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedApiDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/senaisp/MedApi_Deployment/MedApiDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.senaisp.MedApi_Deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApiDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
